aa48ea8334cffb096dbbe93f1138aefbc092122c
Jason <jmurphy.1196@gmail.com>
2021-05-09 01:35:04 -0700

    fixed

--- graphql/apollo-client.ts
+++ graphql/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: `http://${process.env.NEXT_PUBLIC_STRAPI_URL}/graphql`,
+  uri: `https//${process.env.NEXT_PUBLIC_STRAPI_URL}/graphql`,
   cache: new InMemoryCache(),
 });
 
