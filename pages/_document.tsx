5fb1699cf25788345f015561aaf7c02ae85ff400
Jason <jmurphy.1196@gmail.com>
2021-05-09 01:24:58 -0700

    added referrer policy

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,9 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
-        <Head></Head>
+        <Head>
+          <meta name='referrer' content='no-referrer' />
+        </Head>
         <body>
           <Main />
           <NextScript />
